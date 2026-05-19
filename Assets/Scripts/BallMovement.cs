378889f4827f29cb2c9fc94e3d213d457a7d47fe
Nevista75 <nevista75@gmail.com>
2026-05-20 05:07:14 +0700

    Ball Mode Update
    Nambahin mode gameplay Ball tapi belum ada Obstacle apapun.

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float gravityForce = 10f;
+
+    private Rigidbody2D rb;
+
+    private float moveInput;
+
+    private bool gravityUp = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    // Gerak kiri kanan
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>().x;
+    }
+
+    // Toggle gravity dengan Space
+    public void OnToggleGravity(InputValue value)
+    {
+        gravityUp = !gravityUp;
+
+        if (gravityUp)
+        {
+            rb.gravityScale = -gravityForce;
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else
+        {
+            rb.gravityScale = gravityForce;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
