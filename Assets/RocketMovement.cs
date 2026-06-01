63faa04704f4ecc475ab86e617c0210b57a0f31e
Nevista75 <nevista75@gmail.com>
2026-06-01 08:03:47 +0700

    Finalized Prototype
    Integrasi semua level, menambah scene LevelEnd dan Back to Menu, menambah sfx dan bgm Rocket.

--- Assets/RocketMovement.cs
+++ Assets/RocketMovement.cs
@@ -77,6 +77,7 @@ public class RocketMovement : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Obstacle")){
             Debug.Log("Player Mati");
+            AudioManager.Instance.Play(AudioManager.SoundType.Hurt);
             Die();
         } 
     }
