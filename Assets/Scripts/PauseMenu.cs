aca0bf834442a4bd8a14f8feee42e3a740020440
Nevista75 <nevista75@gmail.com>
2026-06-03 16:58:06 +0700

    Added Pause
    Menambahkan pause di setiap mode gameplay

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+
+    private PlayerInput inputActions;
+
+    private bool isPaused = false;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Pause.performed += TogglePause;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Pause.performed -= TogglePause;
+        inputActions.Disable();
+    }
+
+    private void TogglePause(InputAction.CallbackContext context)
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        AudioListener.pause = true;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        AudioListener.pause = false;
+        isPaused = false;
+    }
+
+    public void QuitToMenu()
+    {
+        AudioListener.pause = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
